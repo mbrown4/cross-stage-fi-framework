1d87e59e927e70d941953f6b92666ef40a3df184
mbrown4 <76119151+mbrown4@users.noreply.github.com>
2026-06-02 12:37:22 -0400

    Update FI_DFF.v

--- b01/src/FI_DFF.v
+++ b01/src/FI_DFF.v
@@ -3,7 +3,6 @@
 module FI_DFF_DFRTP_FAULTY (
     input  wire CLK,
     input  wire D,
-    input  wire SET_B,
     input  wire RESET_B,
     input  wire fault_en,
     output reg  Q
@@ -13,7 +12,7 @@ always @(posedge CLK or negedge RESET_B) begin
     if (!RESET_B)
         Q <= 1'b0;
     else if (fault_en)
-        Q <= ~D;   // inject upset at capture edge
+        Q <= ~Q;   // inject upset at capture edge
     else
         Q <= D;
 end
